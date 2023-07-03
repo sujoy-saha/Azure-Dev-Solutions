fb46a719652d541ca237f9d70e4b43c450569033
sujoykumarsaha <sujoykumarsaha@gmail.com>
2023-07-03 10:20:54 +0200

    Adding the solution to git.

---
+++ AzureOpenAIGPT/AzureOpenAIGPTClient.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using static System.Environment;
+
+namespace AzureOpenAIGPT
+{
+    public class AzureOpenAIGPTClient
+    {
+        private readonly OpenAIClient _client;
+        // Enter the deployment name you chose when you deployed the model.
+        private readonly string engine = "REPLACE_WITH_YOUR_MODEL_HERE";
+        private readonly string endpoint = "REPLACE_WITH_YOUR_END_POINT_HERE";
+        private readonly string key = "REPLACE_WITH_YOUR_KEY_VALUE_HERE"; 
+
+        public AzureOpenAIGPTClient()
+        {            
+            _client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+        }
+
+        // Method to send a message to the ChatGPT API and return the response
+        public async Task<string>  SendMessage(string prompt)
+        {
+            try
+            {
+                Response<Completions> completionsResponse = await _client.GetCompletionsAsync(engine, prompt);
+                string completion = completionsResponse.Value.Choices[0].Text;
+                // Extract and return the chatbot's response text
+                return completion ?? string.Empty;
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+           
+        }
+    }
+}
---
+++ AzureOpenAIGPT/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AzureOpenAIGPT.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private AzureOpenAIGPTClient _cptClient = null;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? ChatGPTResponseString { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            
+            _logger = logger;
+        }
+        /*
+        public void OnGet()
+        {
+           
+        }
+        */
+        public async Task OnGetAsync()
+        {
+            if(SearchString != null) {
+                if(_cptClient == null)
+                    _cptClient = new AzureOpenAIGPTClient();
+                ChatGPTResponseString = await _cptClient.SendMessage(SearchString);
+            }
+        }
+    }
+}
